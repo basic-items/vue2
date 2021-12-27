4eae0cac0eab5f1ed9254096af5d52236321f336
liuyi <13403111884@163.com>
2021-12-27 22:56:52 +0800

    '-'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import './registerServiceWorker'
+import route from './router'
+import store from './store'
+import Api from './api'
+import App from './App.vue'
 
+Vue.use(VueRouter)
+Vue.use(Api)
 Vue.config.productionTip = false;
+
+const { routes, AfterEach, BeforeEach } = route
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+router.beforeEach((to: any, from: any, next: any) => {
+  BeforeEach(to, from, next)
+})
+router.afterEach(() => {
+  AfterEach()
+})
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
